df116091a5e74c497639440adf478e5b898eab34
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-21 14:52:36 -0300

    projetoquasepronto

---
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output ,ViewChild,inject} from '@angular/core';
+import { IListItems } from '../../interface/IListItems.interface';
+import { CommonModule, JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+
+    #cdr = inject(ChangeDetectorRef);
+    @ViewChild("inputtext") public inputtext!: ElementRef;
+    @Input({required:true }) public inputListItems: Array<IListItems> = [];
+    @Output() public outputAddListItem = new EventEmitter<IListItems>();
+
+  public focusAndAddItem(value: string){
+
+    if(value){
+      this.#cdr.detectChanges();
+      this.inputtext.nativeElement.value="";
+      const dataAtual = new Date;
+      const timestamp= dataAtual.getTime();
+      const id = `ID ${timestamp}`
+      this.outputAddListItem.emit({
+        id,
+        checked: false,
+        value,
+      })
+      return this.inputtext.nativeElement.focus();
+    }
+
+  }
+}
--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.interface';
+import { CommonModule } from '@angular/common';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  standalone: true,
+  imports: [InputAddItemComponent,CommonModule,InputListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+public addItem = signal(true);
+#setListItems = signal<IListItems[]>([this.#parseItems()]);
+public getListItems = this.#setListItems.asReadonly();
 
+#parseItems(){
+ return JSON.parse(localStorage.getItem('@my-list') || '[]')
+}
+
+public getInputAndAddItem(value: IListItems){
+  localStorage.setItem( '@my-list', JSON.stringify([...this.#setListItems(), value]));
+
+  return this.#setListItems.set(this.#parseItems());
+}
+public deleteallitems(){
+  localStorage.removeItem('@my-list');
+  return this.#setListItems.set(this.#parseItems());
+}
 }
