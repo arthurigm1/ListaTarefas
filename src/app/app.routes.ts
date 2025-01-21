12b80e545805d3eb60222c6d4bf47e1aaa8871b4
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-21 19:56:10 -0300

    construcao pagina login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './modules/to-do-list/pages/list/list.component';
 
+import { LoginComponent } from './moduleslogin/page/login/login.component';
+
 export const routes: Routes = [{
-    path:'',
+    path:'tarefas',
     component: ListComponent,
+},
+{
+        path:'login',
+    component: LoginComponent,
 }
     
 ];
