b995de98daf2530707ba54bf3c39713f318c864f
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-21 21:51:19 -0300

    add layout

--- src/app/moduleslogin/page/layout/layout.component.ts
+++ src/app/moduleslogin/page/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+  @Input() title: string ="";
+  @Input() primaryBtnText: string ="";
+  @Input() secondaryBtnText: string ="";
 }
--- src/app/moduleslogin/page/login/login.component.ts
+++ src/app/moduleslogin/page/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from '../layout/layout.component';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [LayoutComponent],
+  imports: [LayoutComponent, CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  loginForm!: FormGroup;
+constructor(){
+  this.loginForm = new FormGroup({
+    email:new FormGroup('',[Validators.required, Validators.email]),
+    password:new FormGroup('',[Validators.required, Validators.minLength(6)])
+  })
+}
 }
